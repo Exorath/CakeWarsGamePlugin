59f095b479cf7ad49283c7138170b359b7f79b62
Toon Sevrin <twan123@live.be>
2017-05-25 11:50:03 +0200

    Fixed Main reference in FileConfigProvider

--- src/main/java/com/exorath/plugin/game/cakewars/config/FileConfigProvider.java
+++ src/main/java/com/exorath/plugin/game/cakewars/config/FileConfigProvider.java
@@ -1,6 +1,6 @@
 package com.exorath.plugin.game.cakewars.config;
 
-import com.exorath.plugin.kit.Main;
+import com.exorath.plugin.game.cakewars.Main;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
